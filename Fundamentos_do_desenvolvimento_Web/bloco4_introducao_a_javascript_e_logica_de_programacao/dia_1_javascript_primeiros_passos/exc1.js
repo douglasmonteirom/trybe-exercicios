a237ba1a906d9933033b2d34a85cff59fe230596
douglas <douglasmonteiro.m@gmail.com>
2021-07-28 16:25:30 -0300

    Exercicio 1 adicao, subtracao, multiplicacao, divisao, mosulo

---
+++ Fundamentos_do_desenvolvimento_Web/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/exc1.js
@@ -0,0 +1,23 @@
+let numeroA = 15;
+let numeroB = 10;
+let resultado;
+
+//Adição (a + b)
+resultado = numeroA + numeroB;
+console.log(resultado);
+
+//Subtração (a - b)
+resultado = numeroA - numeroB;
+console.log(resultado + '');
+
+//Multiplicação (a * b)
+resultado = numeroA * numeroB;
+console.log(resultado + '');
+
+//Divisão (a / b)
+resultado = numeroA / numeroB;
+console.log(resultado + '');
+
+//Módulo (a % b)
+resultado = numeroA % numeroB;
+console.log(resultado + '');
