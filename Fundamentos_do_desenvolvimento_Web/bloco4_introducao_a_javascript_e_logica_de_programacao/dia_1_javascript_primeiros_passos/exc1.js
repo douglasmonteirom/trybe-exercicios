7b7b89d9e1ebdeecf90c4747042804f3db1e5799
douglas <douglasmonteiro.m@gmail.com>
2021-07-28 16:58:25 -0300

    Incrementa maior de dois números e maior de três números

--- Fundamentos_do_desenvolvimento_Web/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/exc1.js
+++ Fundamentos_do_desenvolvimento_Web/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/exc1.js
@@ -1,5 +1,6 @@
-let numeroA = 15;
-let numeroB = 10;
+let numeroA = 55;
+let numeroB = 25;
+let numeroC = 35;
 let resultado;
 
 //Adição (a + b)
@@ -21,3 +22,23 @@ console.log(resultado + '');
 //Módulo (a % b)
 resultado = numeroA % numeroB;
 console.log(resultado + '');
+
+
+//Faça um programa que retorne o maior de dois números. 
+//Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+if(numeroB>numeroA){
+    console.log('O maior é ' + numeroB)
+}else{
+    console.log('O maior é ' + numeroA)
+}
+
+//Faça um programa que retorne o maior de três números. 
+//Defina no começo do programa três variáveis com os valores que serão comparados.
+if(numeroA > numeroB && numeroA > numeroC){
+    console.log('O maior é o numero A')
+}else if(numeroB >numeroA && numeroB >numeroC){
+    console.log('O maior é o numero B')
+}else{
+    console.log('O maior é o numero C')
+}
