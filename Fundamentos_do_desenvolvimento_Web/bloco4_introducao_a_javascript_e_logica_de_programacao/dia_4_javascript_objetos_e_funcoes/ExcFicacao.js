33af37c1f640119674327d461d8c83eade462187
douglas <douglasmonteiro.m@gmail.com>
2021-08-18 21:16:04 -0300

    Adiciona exercicio de fixacao

---
+++ Fundamentos_do_desenvolvimento_Web/bloco4_introducao_a_javascript_e_logica_de_programacao/dia_4_javascript_objetos_e_funcoes/ExcFicacao.js
@@ -0,0 +1,44 @@
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    }
+}
+
+// console.log(player.name);
+// console.log(player.lastName);
+// console.log(player.age);
+
+// player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log("A jogadora "+player.name+" "+player.lastName+" foi eleita a melhor do mundo por "+ player.bestInTheWorld.length + " vezes");
+// console.log("A jogadora possui "+player.medals.golden+ " medalhas de ouro e "+player.medals.silver+" medalhas de prata");
+
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge'
+};
+
+for (let key in names) {
+    console.log("Ola, "+ names[key]);
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for(let key in car){
+    console.log(key, car[key]);
+  }
