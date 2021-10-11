6b85c7ea4f81c908784760e41efe61aa4b85bc3f
douglas <douglasmonteiro.m@gmail.com>
2021-10-11 16:32:50 -0300

    Faz consulta pokeApi

--- Front_end/bloco_11_introducao_a_react/dia_1_hello_world_no_react/my-pokedex/src/App.js
+++ Front_end/bloco_11_introducao_a_react/dia_1_hello_world_no_react/my-pokedex/src/App.js
@@ -1,15 +1,31 @@
+import React from "react";
 import Pokedex from "./components/Pokedex";
 import pokemons from './data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
-function App() {
-  return (
-    <div>
-      <h1>Pokedex</h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+
+class App extends React.Component {
+  // Usa pokeapi com fetch api
+  // componentDidMount() {
+  //   const apiUrl = "https://pokeapi.co/api/v2/pokemon/500/";
+  //   fetch(apiUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const result = data;
+  //       console.log('This is your data', result);
+  //     });
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Pokedex</h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+
+    )
+  }
 }
 
 export default App;
